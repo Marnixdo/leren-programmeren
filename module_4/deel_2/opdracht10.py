c68331c43f43ea7c4a77d1c210286b14cce24116
Marnix den Otter <99073040@mydavinci.nl>
2023-03-10 10:27:37 +0100

    commit

--- module_4/deel_2/opdracht10.py
+++ module_4/deel_2/opdracht10.py
@@ -1,2 +1,16 @@
 from fruitmand import fruitmand
 
+
+gewicht_lijst = []    
+fruit_lijst = []
+
+for x in fruitmand:
+    gewicht = x['weight'] / 1000
+    fruit_naam = x['name']
+    fruit_lijst.append(fruit_naam)
+    gewicht_lijst.append(gewicht)
+    gewicht_lijst.sort(reverse=True)
+
+print(gewicht_lijst)
+print(fruit_lijst)
+
